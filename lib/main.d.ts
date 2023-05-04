107e81f7bd45c6a848151da49f63ae43852ce7d8
Marek Wilk <marek.wilk@gmail.com>
2023-05-04 23:25:39 +0200

    Add build, remove generated folder

--- lib/main.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const logError: (origin: string, error: unknown) => void;
-export declare const logInfo: (origin: string, error: unknown) => void;
-export declare const logWarning: (origin: string, error: unknown) => void;
-export declare const logDebug: (origin: string, error: unknown) => void;
